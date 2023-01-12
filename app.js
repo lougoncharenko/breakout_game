ee25e4bed6b9f8cf45082656c042001a5f5187f1
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-12 14:13:22 -0500

    made x, y coordinates as variables to make the ball appear to move

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
+// variables
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
-// The ball
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+
 
 // the rectangles on the screen
 ctx.beginPath();
 ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
+ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+ctx.fill();
 ctx.closePath();
+
 
 //function that moves the ball
 function moveTheBall() {
-    //code goes here
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "rgba(194,249,112)";
+    ctx.fill();
+    ctx.closePath();
 }
 setInterval(moveTheBall,10)
