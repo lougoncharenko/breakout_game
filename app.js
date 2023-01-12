fa5a26ea65ed670a1bdb15e2c71342e5d8b84b49
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-12 14:17:08 -0500

    ball moves, but runs off the grid

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-
-
+let dx = 2;
+let dy = -2;
 
 // the rectangles on the screen
 ctx.beginPath();
@@ -15,11 +15,17 @@ ctx.closePath();
 
 
 //function that moves the ball
-function moveTheBall() {
-    ctx.beginPath();
+function drawBall() {
+   ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2);
     ctx.fillStyle = "rgba(194,249,112)";
     ctx.fill();
     ctx.closePath();
 }
-setInterval(moveTheBall,10)
+function moveBall() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall()
+    x += dx;
+    y += dy;
+}
+setInterval(moveBall,10)
