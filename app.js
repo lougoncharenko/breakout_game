e9508246a24702b7906279a6a9510eea8e699c35
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-12 14:32:42 -0500

    ball bounces'

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+const ballRadius = 10;
 
 // the rectangles on the screen
 ctx.beginPath();
@@ -17,7 +18,7 @@ ctx.closePath();
 //function that moves the ball
 function drawBall() {
    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "rgba(194,249,112)";
     ctx.fill();
     ctx.closePath();
@@ -27,5 +28,15 @@ function moveBall() {
     drawBall()
     x += dx;
     y += dy;
+    // Bouncing off the left and right
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+      }
+    // Bouncing off the top and bottom
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+      }
 }
 setInterval(moveBall,10)
+
+
