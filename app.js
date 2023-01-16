90b25203d4a6bc805a05ca8dd38835f9cad89b0d
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-16 12:05:48 -0500

    added a status to the bricks

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ let bricks = [];
 for(let c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
     for(let r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
+        bricks[c][r] = { x: 0, y: 0, status: 1};
     }
 }
 
@@ -50,15 +50,18 @@ function keyUpHandler(e) {
 function drawBricks() {
     for(let c=0; c<brickColumnCount; c++) {
         for(let r=0; r<brickRowCount; r++) {
-            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
-            ctx.fill();
-            ctx.closePath();
+            if (bricks[c][r].status ===1) {
+                let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+                ctx.fill();
+                ctx.closePath(); 
+            }
+         
         }
     }
 }
@@ -80,7 +83,11 @@ function collisionDetection() {
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickColumnCount; r++) {
             const b = bricks[c][r];
-            //calculations
+            //calculations 
+            if (x > b.x && x < b.x + brickWidth && y > b.y &&
+                y < b.y + brickHeight) {
+                    dy = - dy;
+                }  
         }
     }
 }
