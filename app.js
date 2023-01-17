916c5613c43221bf7240ef40819fc955106b63b5
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-17 13:11:01 -0500

    changed double quotes to single quotes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // variables
-const canvas = document.getElementById("myCanvas");
-const scoreBoard = document.getElementById("scoreboard");
-const startButton = document.getElementById("startbutton");
+const canvas = document.getElementById('myCanvas');
+const scoreBoard = document.getElementById('scoreboard');
+const startButton = document.getElementById('startbutton');
 let score = 0;
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
@@ -14,7 +14,7 @@ const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
-//brick variables
+// brick variables
 const brickRowCount = 3;
 const brickColumnCount = 5;
 const brickWidth = 75;
@@ -32,25 +32,25 @@ for(let c=0; c<brickColumnCount; c++) {
 }
 
 //event listeners
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 startButton.addEventListener('click', startGame)
 //functions
 function startGame() {
     const interval = setInterval(playGame,10)
 }
 function keyDownHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
       rightPressed = true;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
       leftPressed = true;
     }
   } 
 function keyUpHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
       rightPressed = false;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
       leftPressed = false;
     }
   }
@@ -70,7 +70,7 @@ function drawBricks() {
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+                ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
                 ctx.fill();
                 ctx.closePath(); 
             }
@@ -81,14 +81,14 @@ function drawBricks() {
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
   }
 function drawBall() {
    ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "rgba(194,249,112)";
+    ctx.fillStyle = 'rgba(194,249,112)';
     ctx.fill();
     ctx.closePath();
 }
