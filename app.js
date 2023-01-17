e5f5c654cd449c2779eee67735827cb5978f30b9
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-17 13:20:17 -0500

    fixed spacing in drawBricks() and changed let to const

--- app.js
+++ app.js
@@ -53,22 +53,21 @@ function mouseMoveHandler(e) {
   }
 }
 function drawBricks() {
-    for(let c=0; c<brickColumnCount; c++) {
-        for(let r=0; r<brickRowCount; r++) {
-            if (bricks[c][r].status ===1) {
-                let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
-                ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
-                ctx.fill();
-                ctx.closePath(); 
-            }
-         
-        }
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status === 1) {
+        const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
+        ctx.fill();
+        ctx.closePath();
+      }
     }
+  }
 }
 function drawPaddle() {
     ctx.beginPath();
