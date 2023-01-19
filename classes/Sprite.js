b52ae44db16bb223ee5f28a6e4e23e4a19d36394
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-19 13:09:45 -0500

    created a sprite class

---
+++ classes/Sprite.js
@@ -0,0 +1,11 @@
+class Sprite {
+  constructor(x = 0, y = 0, width = 100, height = 100, color = '#f00') {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+}
+
+export default Sprite;
