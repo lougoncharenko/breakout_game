90cc4842263799bc800ecb44c5ad7ff7e0d921a6
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-19 13:18:43 -0500

    added a render function to class sprite

--- classes/Sprite.js
+++ classes/Sprite.js
@@ -6,6 +6,14 @@ class Sprite {
     this.height = height;
     this.color = color;
   }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
 export default Sprite;
