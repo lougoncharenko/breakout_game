ed2bcd141416d97512c81f49d421f950eb28271c
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-24 13:38:17 -0500

    finished paddle,draw() method

--- classes/Paddle.js
+++ classes/Paddle.js
@@ -2,7 +2,23 @@
 import Sprite from './Sprite.js';
 
 class Paddle extends Sprite {
+  constructor(x = 0, y = 0, paddleHeight = 10, paddleWidth = 75, color = '#0095DD') {
+    super(x, y, 0, 0, color);
+    this.paddleHeight = paddleHeight;
+    this.paddleWidth = paddleWidth;
+  }
 
+  //   move() {
+
+  //   }
+
+  draw(ctx, canvasWidth, canvasHeight, paddleX) {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvasHeight - this.paddleHeight, this.paddleWidth, this.paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
 export default Paddle;
