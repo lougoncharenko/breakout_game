565720baf465ecf1f637ab0bd362b5e100cae3a9
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-01-24 13:20:38 -0500

    added a x,y arguements in draw function

--- classes/Ball.js
+++ classes/Ball.js
@@ -14,9 +14,9 @@ class Ball extends Sprite {
     this.y += this.dy;
   }
 
-  draw(ctx) { // Overrides the existing render method!
+  draw(ctx, x, y) { // Overrides the existing render method!
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(x, y, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
